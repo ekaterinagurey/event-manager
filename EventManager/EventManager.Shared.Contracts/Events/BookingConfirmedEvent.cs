141f80f7adb3ac9e2f2ad38ec759da760f8ade36
Ekaterina Gurey <ekaterinagurey@gmail.com>
2026-09-22 20:29:22 +0300

    Stage 3. Shared event contract

---
+++ EventManager/EventManager.Shared.Contracts/Events/BookingConfirmedEvent.cs
@@ -0,0 +1,27 @@
+﻿namespace EventManager.Shared.Contracts.Events
+{
+    public record BookingConfirmedEvent
+    {
+        public Guid BookingId { get; init; }
+        public Guid EventId { get; init; }
+        public Guid UserId { get; init; }
+        public int SeatsCount { get; init; }
+        public DateTime ConfirmedAtUtc { get; init; }
+
+        public BookingConfirmedEvent() { }
+
+        public BookingConfirmedEvent(
+            Guid bookingId,
+            Guid eventId,
+            Guid userId,
+            int seatsCount,
+            DateTime confirmedAtUtc)
+        {
+            BookingId = bookingId;
+            EventId = eventId;
+            UserId = userId;
+            SeatsCount = seatsCount;
+            ConfirmedAtUtc = confirmedAtUtc;
+        }
+    }
+}
---
+++ EventManager/EventManager.Shared.Contracts/Topics/KafkaTopics.cs
@@ -0,0 +1,7 @@
+﻿namespace EventManager.Shared.Contracts.Topics
+{
+    public static class KafkaTopics
+    {
+        public const string BookingConfirmed = "booking-confirmed";
+    }
+}
